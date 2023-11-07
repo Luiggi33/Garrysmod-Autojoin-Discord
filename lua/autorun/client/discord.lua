1129f0aa6dada922615d0a46b5141b1ba6c12ad4
Aron <luiggi33@web.de>
2023-11-07 08:18:18 +0100

    rework system

---
+++ lua/autorun/client/discord.lua
@@ -0,0 +1,21 @@
+local discordlink = "https://discord.gg/linkhere"
+local closedelay = 4
+
+hook.Add("InitPostEntity", "StartDiscordPanel", function()
+	timer.Simple(1, function()
+		local frame = vgui.Create("DFrame") 
+		frame:KillFocus() 
+		frame:SetAlpha(255) 
+		frame:SetSize(0,0)
+		frame:Center() 
+		frame:MakePopup() 
+		local html = vgui.Create("HTML", frame) 
+		html:Dock(FILL)
+		html:KillFocus() 
+		html:OpenURL(discordlink) 
+		frame:Hide() 
+		timer.Simple(closedelay, function() 
+			frame:Close() 
+		end)
+	end)
+end)
